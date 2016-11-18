347efb9d3c42d116eff81958a29eb514d1bb67bd
Rob DelValle <robdel13@gmail.com>
2016-11-17 19:31:03 -0700

    Angular  and  implimented with ng-grid for an itunes api call

--- js/itunesService.js
+++ js/itunesService.js
@@ -1,14 +1,39 @@
 angular.module('itunes').service('itunesService', function($http, $q){
   //This service is what will do the 'heavy lifting' and get our data from the iTunes API.
-  //Also note that we're using a 'service' and not a 'factory' so all your methods you want to call in your controller need to be on 'this'.
+  //Also note that we're using a 'service' and not a 'factory' so all your methods you want to call in your controller 
+  //need to be on 'this'.
 
-  //Write a method that accepts an artist's name as the parameter, then makes a 'JSONP' http request to a url that looks like this
+  //Write a method that accepts an artist's name as the parameter, then makes a 'JSONP' http request to a url that 
+  //looks like this
   //https://itunes.apple.com/search?term=' + artist + '&callback=JSON_CALLBACK'
   //Note that in the above line, artist is the parameter being passed in. 
-  //You can return the http request or you can make your own promise in order to manipulate the data before you resolve it.
+  //You can return the http request or you can make your own promise in order to manipulate the data before you 
+  //resolve it.
 
     //Code here
-    
+    this.getArtist = function(artist) {
+        var deffered = $q.defer();
+         $http({
+          method: 'JSONP',
+          url: 'https://itunes.apple.com/search?term=' + artist + '&callback=JSON_CALLBACK'
+        }).then(function(response) {
+          var parsedResponse = response.data.results;
+          var parsedLength = parsedResponse.length;
+          var responseArray = [];
+          for(var i = 0; i < parsedLength; i++) {
+            responseArray.push({
+              AlbumArt: parsedResponse[i].artworkUrl100,
+              Artist: parsedResponse[i].artistName,
+              Collection: parsedResponse[i].collectionName,
+              CollectionPrice: parsedResponse[i].collectionPrice,
+              Play: parsedResponse[i].previewUrl,
+              Type: parsedResponse[i].kind
+            });
+          }
+          deffered.resolve(responseArray)
+        })
+        return deffered.promise;
+    };
 
 
 
@@ -24,8 +49,13 @@ angular.module('itunes').service('itunesService', function($http, $q){
       Play: "http://a423.phobos.apple.com/us/r1000/013/Music4/v4/4a/ab/7c/4aab7ce2-9a72-aa07-ac6b-2011b86b0042/mzaf_6553745548541009508.plus.aac.p.m4a"
       Type: "song"
   */
-  //the iTunes API is going to give you a lot more details than ng-grid wants. Create a new array and then loop through the iTunes data pushing into your new array objects that look like the above data. Make sure your method returns this finalized array of data. 
+  //the iTunes API is going to give you a lot more details than ng-grid wants. Create a new array and then loop through 
+  //the iTunes data pushing into your new array objects that look like the above data. Make sure your method returns this 
+  //finalized array of data. 
   // When this is complete, head back to your controller.
+
+
+
 
 
 });
